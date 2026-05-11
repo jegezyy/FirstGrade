87d744fda357a964bedc049294951b84bf1f5568
jegezyy <idc77585850@gmail.com>
2026-05-11 09:26:04 +0300

    supporting main.py sqlite3 -> postgresql slave db

--- supporting/main.py
+++ supporting/main.py
@@ -9,11 +9,11 @@ load_dotenv(dotenv_path=os.path.join('..', '.env'))
 app = FastAPI()
 
 DB_CONFIG = {
-    'dbname': os.getenv('DB_NAME'),
+    'dbname': os.getenv('DB_SLAVE_NAME'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST'),
-    'port': os.getenv('DB_PORT')
+    'host': os.getenv('DB_SLAVE_HOST'),
+    'port': os.getenv('DB_SLAVE_PORT')
 }
 
 
